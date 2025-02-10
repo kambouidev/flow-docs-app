fe1a8465dedece1c2233bce50f619bfa679b3557
kambouidev <mohamed@squaads.com>
2025-02-10 22:05:53 +0000

    ✨ feat: create header component

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface HeaderProps {
+    title: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>{title}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 20,
+        backgroundColor: "white",
+    },
+    headerTitle: {
+        fontSize: 22,
+        fontWeight: "bold",
+    },
+});
+
+export default Header;
