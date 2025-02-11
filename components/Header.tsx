c65dd3d8a2a5700e7b642565d49f38519d83ec84
kambouidev <mohamed@squaads.com>
2025-02-11 23:27:46 +0000

    🔄️ refactor: add onClose option in header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,21 @@
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 interface HeaderProps {
     title: string;
+    onClose?: () => void
 }
 
-const Header: React.FC<HeaderProps> = ({ title }) => {
+const Header: React.FC<HeaderProps> = ({ title, onClose }) => {
     return (
         <View style={styles.header}>
             <Text style={styles.headerTitle}>{title}</Text>
+            {onClose && <View>
+                <TouchableOpacity style={styles.backButton} onPress={onClose}>
+                    <Ionicons name="close" size={24} color="black" />
+                </TouchableOpacity>
+            </View>}
         </View>
     );
 };
@@ -18,13 +25,17 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        padding: 20,
         backgroundColor: "white",
     },
     headerTitle: {
-        fontSize: 28,
+        fontSize: 24,
         fontWeight: "bold",
+        padding: 16,
+
     },
+    backButton: {
+        padding: 16,
+    }
 });
 
 export default Header;
