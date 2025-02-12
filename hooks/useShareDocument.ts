a88051f344de124a3dba55d16104aa260e7bd8c6
kambouidev <mohamed@squaads.com>
2025-02-12 22:47:03 +0000

    ✨ feat: create useShareDocument hook to share documents (not really)

---
+++ hooks/useShareDocument.ts
@@ -0,0 +1,23 @@
+import { Share } from 'react-native';
+import { IDocument } from '../types/IDocument';
+import { formatDate, generateLink } from '../services/utils';
+
+export function useShareDocument() {
+  const shareDocument = async (document: IDocument) => {
+    try {
+      const message = `
+      🔗 Link: ${generateLink()}
+      📄 ${document.Title} (v${document.Version})
+      📅 CreatedAt: ${formatDate(document.CreatedAt)}
+      📝 Contributors: ${document.Contributors.map((c) => c.Name).join(', ')}
+      📎 Attachments: ${document.Attachments.join(', ')}
+        `;
+
+      await Share.share({ message });
+    } catch (error) {
+      console.error('Error sharing:', error);
+    }
+  };
+
+  return { shareDocument };
+}
