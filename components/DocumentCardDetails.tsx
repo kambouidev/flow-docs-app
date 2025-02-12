293879bed17fe2a7f9eb0ad5036c4355f5dbb5ad
kambouidev <mohamed@squaads.com>
2025-02-12 22:48:11 +0000

    🔄️ refactor: DocumentCardDetails to add button to share document and add text to show when document was created

--- components/DocumentCardDetails.tsx
+++ components/DocumentCardDetails.tsx
@@ -1,27 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { IDocument } from '../types/IDocument';
+import { Entypo } from '@expo/vector-icons';
+import { timeAgo } from '../services/utils';
 
 interface DocumentCardDetailsProps {
-    document: IDocument
+    document: IDocument,
+    handleShareDocument: (document: IDocument) => void
 }
 
 
 
-export const DocumentCardDetails: React.FC<DocumentCardDetailsProps> = ({ document: { Attachments, Contributors, Title, Version } }) => (
+export const DocumentCardDetails: React.FC<DocumentCardDetailsProps> = ({ document, handleShareDocument }) => (
     <View style={styles.card}>
-        <Text style={styles.cardTitle}>
-            {Title} <Text style={styles.cardVersion}>Version {Version}</Text>
-        </Text>
+        <View style={styles.cardHeader}>
+            <Text style={styles.cardTitle}>
+                {document.Title} <Text style={styles.cardVersion}>Version {document.Version}</Text>
+            </Text>
+            <TouchableOpacity style={styles.shareButton} onPress={() => handleShareDocument(document)}>
+                <Entypo name="share" size={24} color="blue" />
+            </TouchableOpacity>
+        </View>
+
         <View style={styles.cardcontent}>
             <View style={{ flex: 1 }}>
                 <Text style={styles.cardSubtitle}>Contributors</Text>
-                {Contributors.map(({ ID, Name }) => <Text key={ID}>{Name}</Text>)}
+                {document.Contributors.map(({ ID, Name }) => <Text key={ID}>{Name}</Text>)}
             </View>
             <View style={{ flex: 1 }}>
                 <Text style={styles.cardSubtitle}>Attachments</Text>
-                {Attachments.map((file, index) => <Text key={index}>{file}</Text>)}
+                {document.Attachments.map((file, index) => <Text key={index}>{file}</Text>)}
             </View>
+
+        </View>
+        <View style={{ flex: 1, marginTop: 5 }}>
+            <Text style={styles.timeAgo}>{timeAgo(document.CreatedAt)}</Text>
         </View>
     </View>
 );
@@ -38,9 +51,18 @@ const styles = StyleSheet.create({
         shadowRadius: 4,
         elevation: 2
     },
+    cardHeader: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
     cardTitle: {
         fontSize: 16,
         fontWeight: "bold"
+    },
+    shareButton: {
+        padding: 5,
+        borderRadius: 5
     },
     cardcontent: {
         flex: 1,
@@ -54,5 +76,10 @@ const styles = StyleSheet.create({
     cardSubtitle: {
         fontWeight: "bold",
         marginTop: 5
+    },
+    timeAgo: {
+        fontWeight: "400",
+        fontSize: 10,
+        textAlign: 'right'
     },
 });
