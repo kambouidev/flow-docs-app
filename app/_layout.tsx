e6c0e5c0683c2a736923f77ca2597c5d02057737
kambouidev <mohamed@squaads.com>
2025-02-13 21:35:54 +0000

    ✨ feat: wrap Stack component with Jotai Provider in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
+import { Provider } from "jotai";
 import { SafeAreaView, StyleSheet } from "react-native";
 
 export default function RootLayout() {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Stack screenOptions={{ headerShown: false }} />
+      <Provider>
+        <Stack screenOptions={{ headerShown: false }} />
+      </Provider>
     </SafeAreaView>
   );
 }
