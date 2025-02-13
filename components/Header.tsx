4ffde63633989f0223760b6308ee04a6dac967be
kambouidev <mohamed@squaads.com>
2025-02-13 18:12:29 +0000

    🔄️ feat: add navigation buttons in header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,32 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 interface HeaderProps {
     title: string;
     onClose?: () => void
+    goToNotifications?: () => void
+    backButton?: () => void
 }
 
-const Header: React.FC<HeaderProps> = ({ title, onClose }) => {
+const Header: React.FC<HeaderProps> = ({ title, onClose, backButton, goToNotifications }) => {
     return (
         <View style={styles.header}>
-            <Text style={styles.headerTitle}>{title}</Text>
+            <View style={styles.headerLeft}>
+                {backButton && <View>
+                    <TouchableOpacity style={styles.backButton} onPress={backButton}>
+                        <Ionicons name="chevron-back" size={24} color="black" />
+                    </TouchableOpacity>
+                </View>}
+                <Text style={styles.headerTitle}>{title}</Text>
+            </View>
             {onClose && <View>
                 <TouchableOpacity style={styles.backButton} onPress={onClose}>
                     <Ionicons name="close" size={24} color="black" />
+                </TouchableOpacity>
+            </View>}
+            {goToNotifications && <View>
+                <TouchableOpacity style={styles.notificationsButton} onPress={goToNotifications}>
+                    <Ionicons name="notifications-outline" size={24} color="black" />
+                    <View style={styles.dot}>
+                        <Text style={styles.dotText}>99</Text>
+                    </View>
                 </TouchableOpacity>
             </View>}
         </View>
@@ -27,6 +44,10 @@ const styles = StyleSheet.create({
         alignItems: "center",
         backgroundColor: "white",
     },
+    headerLeft: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
     headerTitle: {
         fontSize: 24,
         fontWeight: "bold",
@@ -35,6 +56,25 @@ const styles = StyleSheet.create({
     },
     backButton: {
         padding: 16,
+    },
+    notificationsButton: {
+        padding: 10,
+        borderWidth: 1,
+        marginRight: 10,
+        borderRadius: 10
+    },
+    dot: {
+        borderRadius: 15,
+        position: "absolute",
+        backgroundColor: 'blue',
+        right: 8,
+        top: 5,
+    },
+    dotText: {
+        textAlign: "center",
+        padding: 2,
+        color: 'white',
+        fontSize: 10
     }
 });
 
