d7bcbe0feba4f575fe3c9dad0881b805bddb4860
kambouidev <mohamed@squaads.com>
2025-02-13 22:47:31 +0000

    ✨ feat: wrap RootLayout with QueryClientProvider for React Query integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { Provider } from "jotai";
 import { SafeAreaView, StyleSheet } from "react-native";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <Provider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </Provider>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView style={styles.safeArea}>
+        <Provider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </Provider>
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
 
