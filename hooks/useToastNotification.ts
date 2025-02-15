27c0153f7e8874a53f9acd46daf198dd23c7ff19
kambouidev <mohamed@squaads.com>
2025-02-15 16:43:55 +0000

    ✨ feat: add sound notification for toast messages in useToastNotification hook

--- hooks/useToastNotification.ts
+++ hooks/useToastNotification.ts
@@ -2,19 +2,51 @@ import Toast from 'react-native-toast-message';
 import { useNotificationsStore } from '../stores/notifications.store';
 import { INotification } from '../types/INotification';
 import { useRouter, useSegments } from 'expo-router';
-import { useEffect } from 'react';
+import { useEffect, useCallback, useRef } from 'react';
+import { Audio } from 'expo-av';
 
 export function useToastNotification() {
   const { addNotification } = useNotificationsStore();
   const router = useRouter();
   const segment = useSegments();
+  const soundRef = useRef<Audio.Sound>();
+
+  useEffect(() => {
+    const loadSound = async () => {
+      try {
+        const { sound } = await Audio.Sound.createAsync(require('../assets/sounds/toast-notification.wav'));
+        soundRef.current = sound;
+      } catch (error) {
+        console.error('Error cargando sonido:', error);
+      }
+    };
+
+    loadSound();
+
+    return () => {
+      if (soundRef.current) {
+        soundRef.current.unloadAsync();
+      }
+    };
+  }, []);
 
   useEffect(() => {
     Toast.hide();
   }, [segment]);
 
-  const showNotification = (newNotification: INotification) => {
+  const playNotificationSound = useCallback(async () => {
+    try {
+      if (soundRef.current) {
+        await soundRef.current.replayAsync();
+      }
+    } catch (error) {
+      console.error('Error playing sound:', error);
+    }
+  }, []);
+
+  const showNotification = async (newNotification: INotification) => {
     addNotification(newNotification);
+    await playNotificationSound();
     Toast.show({
       type: 'newNotification',
       autoHide: true,
